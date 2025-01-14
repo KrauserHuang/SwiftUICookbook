7b4246d073d7d490d32a2b60420d4a030094910d
Andrew Huang <vivatv.it0001@gmail.com>
2025-01-14 13:45:31 +0800

    Initial Commit

---
+++ SwiftUICookbook/SwiftUICookbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICookbookApp.swift
+//  SwiftUICookbook
+//
+//  Created by IT-MAC-02 on 2025/1/14.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICookbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
