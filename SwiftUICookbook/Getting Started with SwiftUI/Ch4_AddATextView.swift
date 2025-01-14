e618a7f76def9a20254071ce20612704c28bbd8b
Andrew Huang <vivatv.it0001@gmail.com>
2025-01-14 14:37:34 +0800

    [Feature] Sec1-Ch4: Add a Text View in SwiftUI

---
+++ SwiftUICookbook/Getting Started with SwiftUI/Ch4_AddATextView.swift
@@ -0,0 +1,19 @@
+//
+//  Ch4_AddATextView.swift
+//  SwiftUICookbook
+//
+//  Created by IT-MAC-02 on 2025/1/14.
+//
+
+import SwiftUI
+
+struct Ch4_AddATextView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .font(.headline)
+    }
+}
+
+#Preview {
+    Ch4_AddATextView()
+}
