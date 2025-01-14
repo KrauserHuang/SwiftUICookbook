5bcb345fa5458d1da0b261501d80ccab558a76c5
Andrew Huang <vivatv.it0001@gmail.com>
2025-01-14 14:21:39 +0800

    [Feature] Sec1-Ch2: Understand SwiftUI Views & View Hierarchies
    1. Add Kodeco image

---
+++ SwiftUICookbook/Getting Started with SwiftUI/Ch2_UnderstandViewHierarchies.swift
@@ -0,0 +1,21 @@
+//
+//  Ch2_UnderstandViewHierarchies.swift
+//  SwiftUICookbook
+//
+//  Created by IT-MAC-02 on 2025/1/14.
+//
+
+import SwiftUI
+
+struct Ch2_UnderstandViewHierarchies: View {
+    var body: some View {
+        VStack {
+            Image(.kodeco)
+            Text("Welcome to Kodeco SwiftUI Cookbook!")
+        }
+    }
+}
+
+#Preview {
+    Ch2_UnderstandViewHierarchies()
+}
