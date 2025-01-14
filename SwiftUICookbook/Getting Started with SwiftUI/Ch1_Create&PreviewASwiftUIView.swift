709dea078b8b739289992fc2bfa96e3fac704988
Andrew Huang <vivatv.it0001@gmail.com>
2025-01-14 14:14:13 +0800

    [Feature] Create section1 folder Getting Started with SwiftUI
    1. Chapter1: Create & Preview a SwiftUI View

---
+++ SwiftUICookbook/Getting Started with SwiftUI/Ch1_Create&PreviewASwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  Ch1_Create&PreviewASwiftUIView.swift
+//  SwiftUICookbook
+//
+//  Created by IT-MAC-02 on 2025/1/14.
+//
+
+import SwiftUI
+
+struct Ch1_Create_PreviewASwiftUIView: View {
+    var body: some View {
+        Text("Hello, SwiftUI!")
+    }
+}
+
+#Preview {
+    Ch1_Create_PreviewASwiftUIView()
+}
