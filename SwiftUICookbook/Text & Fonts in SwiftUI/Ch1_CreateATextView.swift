23b588198c15b742b1d614628bcb59627e0e82d1
Andrew Huang <vivatv.it0001@gmail.com>
2025-01-22 15:02:54 +0800

    [Feature] Sec4-Ch1: Create a Text View in SwiftUI

---
+++ SwiftUICookbook/Text & Fonts in SwiftUI/Ch1_CreateATextView.swift
@@ -0,0 +1,22 @@
+//
+//  Ch1_CreateATextView.swift
+//  SwiftUICookbook
+//
+//  Created by IT-MAC-02 on 2025/1/22.
+//
+
+import SwiftUI
+
+struct Ch1_CreateATextView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+            .font(.title)
+            .foregroundStyle(.blue)
+            .background(.black)
+    }
+}
+
+#Preview {
+    Ch1_CreateATextView()
+}
